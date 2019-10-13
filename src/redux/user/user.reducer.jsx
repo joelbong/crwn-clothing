fd34693b0914bf72e6da103273b4acd3597428e0
Andre <joelbong@hotmail.com>
2019-10-14 00:09:31 +0200

    Rederict to home on sign in;

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,13 +1,15 @@
+import {UserActionTypes} from './user.types'
+
 const INITIAL_STATE = {
-    current_user: null,
+    currentUser: null,
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return  {
                 ...state,
-                current_user: action.payload
+                currentUser: action.payload
             }
         default:
             return state;
